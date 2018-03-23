a04943d40a42c7bcb3d19846554d3bcc48f06b9e
Djordje Nikolic <djole.nikolic.priv@gmail.com>
2018-03-23 17:15:38 +0100

    adding more options

--- src/IronImage/IronImage.js
+++ src/IronImage/IronImage.js
@@ -4,27 +4,43 @@ import './IronImage.css';
 class IronImage extends Component {
   state = {
     imageLink: '',
-    fadeClass: ''
+    fadeClass: '',
+    transitionDuration: this.props.transitionDuration || '1s',
+    displayPreload: 'absolute'
   };
 
   handleImageLoad = () => {
       this.setState({
           imageLink: this.props.src,
-          fadeClass: 'iron-image-fade-in'
+          fadeClass: 'iron-image-fade-in',
       });
+      setTimeout(() => {
+        this.setState({
+            displayPreload: 'none'
+        });
+      }, this.state.transitionDuration);
   };
 
-  render() {
+    render() {
     return (
       <div className="iron-image-container">
-        <img alt={this.props.alt}
+        <img
+             alt={this.props.alt}
              src={this.props.src}
              className={`iron-image-loaded ${this.state.fadeClass}`}
+             style={{
+                  transition: `opacity ${this.state.transitionDuration} ease`
+             }}
              onLoad={this.handleImageLoad}
         />
-        <img alt={this.props.alt}
+        <img
+             alt={this.props.alt}
              src={this.props.srcPreload}
-             className="iron-image-preload" />
+             className="iron-image-preload"
+             style={{
+                 display: this.state.displayPreload
+             }}
+        />
       </div>
     )
   }
