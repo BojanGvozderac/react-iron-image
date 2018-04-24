db3fd6f3f983f86f79924dec678c65ed7b121417
Bojan Gvozderac <bojan.gvozderac@jsguru.io>
2018-04-24 23:31:35 +0200

    initial project setup

---
+++ src/lib/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './style.css';
+
+const IMAGE_FADE_IN_CLASS = `iron-image--fade-in`;
+
+class IronImage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageLoadFinishedClass: ``,
+      placeholderStyle: { backgroundImage: `url(${props.placeholder})` },
+    };
+  }
+
+  imageLoadHandler() { this.setState({ imageLoadFinishedClass: IMAGE_FADE_IN_CLASS }); }
+
+  render() {
+    return (
+      <div
+        className="iron-image__container"
+        style={this.state.placeholderStyle}
+      >
+        <img
+          className={`iron-image ${this.state.imageLoadFinishedClass}`}
+          alt={this.props.alt}
+          src={this.props.src}
+          style={this.state.imageStyle}
+          onLoad={this.imageLoadHandler}
+        />
+      </div>
+    );
+  };
+}
+
+export default IronImage;
